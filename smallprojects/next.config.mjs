09cfdfb7e9ca273173ade9dfaad0305f603be8d1
Madchili <jonas_nl@live.se>
2025-03-13 21:08:23 +0100

    fix 13

--- smallprojects/next.config.mjs
+++ smallprojects/next.config.mjs
@@ -1,4 +1,16 @@
+import { resolve } from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': resolve(__dirname, 'app/components'),
+      '@context': resolve(__dirname, 'app/context'),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
+
