f683da7445bbe3dd690d9f744e793ea500ee5302
Madchili <jonas_nl@live.se>
2025-03-13 21:39:19 +0100

    fix 101

--- smallprojects/next.config.mjs
+++ smallprojects/next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
-import { resolve } from 'path';
 
-
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@context': resolve(process.cwd(), 'app/context'),
-      '@components': resolve(process.cwd(), 'app/components'),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
